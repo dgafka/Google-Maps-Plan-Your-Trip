41a9a4a46ea9dc215c54c5b8c10572883235a005
dgafka <dariss6666@gmail.com>
2014-08-15 09:26:46 +0200

    Update auth.js

--- configuration/auth.js
+++ configuration/auth.js
@@ -5,21 +5,21 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID' 		: '317621851722391', // your App ID
-        'clientSecret' 	: '7a3a94d090d099e17eeb5f4322275cb0', // your App Secret
-        'callbackURL' 	: 'http://95.85.9.19:3000/auth/facebook/callback'
+        'clientID' 		: '*', // your App ID
+        'clientSecret' 	: '*', // your App Secret
+        'callbackURL' 	: 'http://127.0.0.1:3000/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey' 		: '3bLFV24YVVBXwr00X5GaFba6Y',
-        'consumerSecret' 	: 'lqtsXx14e39oSy69kwvsiHTsj9obMVYbo5HOtMn5Xwa6j2gZku',
-        'callbackURL' 		: 'http://95.85.9.19:3000/auth/twitter/callback'
+        'consumerKey' 		: '*',
+        'consumerSecret' 	: '*',
+        'callbackURL' 		: 'http://127.0.0.1::3000/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID' 		: '408946951273-jr8ccdg2vcorbkgkfc1takjeimar09a5.apps.googleusercontent.com',
-        'clientSecret' 	: 'Uacdd2vLJE4LZBM2-05SEdb3',
-        'callbackURL' 	: 'http://95.85.9.19:3000/auth/google/callback'
+        'clientID' 		: '*',
+        'clientSecret' 	: '*',
+        'callbackURL' 	: 'http://127.0.0.1::3000/auth/google/callback'
     }
 
 };
